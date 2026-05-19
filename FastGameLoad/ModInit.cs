9ddf5b918bad48dc46afb7e60ada42e36892cd18
reinzer <reynfull@gmail.com>
2026-05-19 16:21:31 +0300

    Init commit

---
+++ FastGameLoad/ModInit.cs
@@ -0,0 +1,50 @@
+﻿using BepInEx;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using System.Reflection;
+using HarmonyLib;
+using Unity.Mathematics;
+using Newtonsoft.Json;
+using GlobalEnums;
+using System.Collections;
+using HutongGames.PlayMaker.Actions;
+using GenericVariableExtension;
+using HutongGames.PlayMaker;
+using Unity.Burst.Intrinsics;
+
+namespace FastGameLoad
+{
+    [BepInPlugin("bepinex.plugin.FastGameLoad", "FastGameLoad", "0.1.0")]
+    public class FastGameLoad : BaseUnityPlugin
+    {
+        public static FastGameLoad instance;
+		public static bool isAnimFast = false;
+		public static BepInEx.Logging.ManualLogSource Log;
+        private void Awake()
+        {
+            instance = this;
+			Log = this.Logger;
+
+            var harmony = new Harmony("com.fastgamestartup");
+            Harmony.CreateAndPatchAll(typeof(FastGameLoad));
+
+            Logger.LogInfo($"Plugin is loaded!");
+        }
+		
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(StartManager), "Start")]
+        public static IEnumerator Start_Manager_Start_Postfix(IEnumerator __result, StartManager __instance)
+        {
+			if(!FastGameLoad.isAnimFast){
+				__instance.startManagerAnimator.speed = float.MaxValue;
+				FastGameLoad.Log.LogInfo("START ANIM IS FAST NOW");
+				FastGameLoad.isAnimFast = true;
+			}
+			while(__result.MoveNext()){
+				yield return __result.Current;
+			}
+        }
+    }
+}
